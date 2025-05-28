b96742c981bc34d165695c78010e60f757ce8201
sharanya67468 <sharanyas603@gmail.com>
2025-05-28 14:36:56 +0530

    hiii

---
+++ vote.js
@@ -0,0 +1,12 @@
+function vote(){
+    
+  var name=document.getElementById("name").value;
+  var age=document.getElementById("age").value;
+  var res=document.getElementById("res")
+  if(age>18){
+    res.innerHTML="you are eligible👌";
+  }
+  else{
+    res.innerHTML="you are not eligible";
+  }
+}
